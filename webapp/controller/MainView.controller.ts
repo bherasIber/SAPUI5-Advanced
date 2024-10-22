11a49f138f6bd818d5bfd20c1dbf54729e6851ff
bherasIber <bheras@ibercacao.es>
2024-10-22 15:01:37 +0000

    componentes_dinamicos

--- webapp/controller/MainView.controller.ts
+++ webapp/controller/MainView.controller.ts
@@ -7,6 +7,9 @@ import ResourceModel from "sap/ui/model/resource/ResourceModel"; // Asegúrate d
 import FilterOperator from "sap/ui/model/FilterOperator";
 import Filter from "sap/ui/model/Filter";
 import MessageToast from "sap/m/MessageToast";
+import ColumnListItem from "sap/m/ColumnListItem";
+import Table from "sap/m/Table";
+import Column from "sap/m/Column";
 
 
 interface ICustomResourceBundle {
@@ -89,16 +92,100 @@ export default class MainView extends Controller {
 
     public onShowCity(): void {
         var oJSONModelConfig = this.getView()?.getModel("jsonModelConfig");
-        oJSONModelConfig.setProperty("/visibleCity", true);
-        oJSONModelConfig.setProperty("/visibleBtnShowCity", false);
-        oJSONModelConfig.setProperty("/visibleBtnHideCity", true);
+        oJSONModelConfig?.setProperty("/visibleCity", true);
+        oJSONModelConfig?.setProperty("/visibleBtnShowCity", false);
+        oJSONModelConfig?.setProperty("/visibleBtnHideCity", true);
     }
 
     public onHideCity(): void {
         var oJSONModelConfig = this.getView()?.getModel("jsonModelConfig");
-        oJSONModelConfig.setProperty("/visibleCity", false);
-        oJSONModelConfig.setProperty("/visibleBtnShowCity", true);
-        oJSONModelConfig.setProperty("/visibleBtnHideCity", false);
+        oJSONModelConfig?.setProperty("/visibleCity", false);
+        oJSONModelConfig?.setProperty("/visibleBtnShowCity", true);
+        oJSONModelConfig?.setProperty("/visibleBtnHideCity", false);
+    }
+
+    public showOrders(oEvent): void {
+        var ordersTable = this.getView()?.byId("ordersTable");
+        ordersTable?.destroyItems();
+
+        var itemPressed = oEvent.getSource();
+        var oContext = itemPressed.getBindingContext("jsonEmployees");
+
+        var objectContext = oContext.getObject();
+        var orders = objectContext.Orders;
+
+        var ordersItems = [];
+
+        for (var i in orders) {
+            ordersItems.push(new ColumnListItem({
+                cells: [
+                    new Label({ text: orders[i].OrderID}),
+                    new Label({ text: orders[i].Freight}),
+                    new Label({ text: orders[i].ShipAddress})
+                ]
+            }));
+        }
+
+        var newTable = new Table({
+            width: "auto",
+            columns: [
+                new Column({header: new Label({text: "{i18n>orderID}"})}),
+                new Column({header: new Label({text: "{i18n>freight}"})}),
+                new Column({header: new Label({text: "{i18n>shipAddress}"})})
+            ],
+            items: ordersItems
+        }).addStyleClass("sapUiSmallMargin");
+
+        ordersTable?.addItem(newTable);
+
+
+        var newTableJSON = new Table();
+        newTableJSON.setWidth("auto");
+        newTableJSON.addStyleClass("sapUiSmallMargin");
+
+        var columnOrderID = new Column();
+        var labelOrderID = new Label();
+        labelOrderID.bindProperty("text","i18n>orderID");
+        columnOrderID.setHeader(labelOrderID);
+        newTableJSON.addColumn(columnOrderID);
+        
+        var columnFreight = new Column();
+        var labelFreight = new Label();
+        labelFreight.bindProperty("text","i18n>freight");
+        columnFreight.setHeader(labelFreight);
+        newTableJSON.addColumn(columnFreight);
+
+        var columnShipAddress = new Column();
+        var labelShipAddress = new Label();
+        labelShipAddress.bindProperty("text","i18n>shipAddress");
+        columnShipAddress.setHeader(labelShipAddress);
+        newTableJSON.addColumn(columnShipAddress);
+
+        var columnListItem = new ColumnListItem();
+
+        var cellOrderID = new Label();
+        cellOrderID.bindProperty("text","jsonEmployees>OrderID");
+        columnListItem.addCell(cellOrderID);
+
+        var cellFreight = new Label();
+        cellFreight.bindProperty("text","jsonEmployees>Freight");
+        columnListItem.addCell(cellFreight);
+
+        var cellShipAddress = new Label();
+        cellShipAddress.bindProperty("text","jsonEmployees>ShipAddress");
+        columnListItem.addCell(cellShipAddress);
+
+        var oBindingInfo = {
+            model: "jsonEmployees",
+            path: "Orders",
+            template: columnListItem
+        };
+
+        newTableJSON.bindAggregation("items", oBindingInfo);
+        newTableJSON.bindElement("jsonEmployees>" + oContext.getPath());
+
+        ordersTable?.addItem(newTableJSON);
+
     }
     
     public onValidate(): void {
