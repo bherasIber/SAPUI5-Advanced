a9e783a75983bb18b5d1abac573bfb9570ef70e7
bherasIber <bheras@ibercacao.es>
2024-11-26 16:49:01 +0000

    Añadir complemento para firma

--- Employees/webapp/controller/OrderDetails.controller.js
+++ Employees/webapp/controller/OrderDetails.controller.js
@@ -1,15 +1,42 @@
 // @ts-nocheck
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/routing/History"
-], function (Controller, History) {
+    "sap/ui/core/routing/History",
+    "sap/m/MessageBox"
+], function (Controller, History, MessageBox) {
 
     function _onObjectMatched(oEvent) {
+        this.onClearSignature();
+
         this.getView().bindElement({
             path: "/Orders(" + oEvent.getParameter("arguments").OrderID + ")",
-            model: "odataNorthwind"
-         })
-    }
+            model: "odataNorthwind",
+            events : {
+                dataReceived: function (oData) {
+                    _readSignature.bind(this)(oData.getParameter("data").OrderID, oData.getParameter("data").EmployeeID);
+                }.bind(this)
+            }
+        });
+
+        const objContext = this.getView().getModel("odataNorthwind").getContext("/Orders(" + oEvent.getParameter("arguments").OrderID + ")").getObject();
+        if(objContext){
+            _readSignature.bind(this)(objContext.OrderID, objContext.EmployeeID);
+        };
+    };
+
+    function _readSignature(orderId, emplyeeId){
+        this.getView().getModel("incidenceModel").read("/SignatureSet(OrderId='" + orderId + "',SapId='" + this.getOwnerComponent().SapId + "',EmployeeId='" + emplyeeId + "')", {
+            success: function(data){
+                const signature = this.getView().byId("signature");
+                if(data.MediaContent !== ""){
+                    signature.setSignature("data:image/png;base64," + data.MediaContent);
+                };
+            }.bind(this),
+            error: function(data){
+
+            }
+        })
+    };
 
     return Controller.extend("logaligroup.Employees.controller.OrderDetails", {
         onInit: function () {
@@ -52,7 +79,7 @@ sap.ui.define([
                     content: [
                         new sap.m.Bar({
                             contentLeft: new sap.m.Label({text: "{odataNorthwind>/Products(" + contextObject.ProductID + ")/ProductName} ({odataNorthwind>Quantity})"}),
-                            contentMiddle: new sap.m.ObjectStatus({text: "{i18n>avaliableStock} {odataNorthwind>/Products(" + contextObject.ProductID + ")/UnitsInStock}", state:"Error"}),
+                            contentMiddle: new sap.m.ObjectStatus({text: "{i18n>availableStock} {odataNorthwind>/Products(" + contextObject.ProductID + ")/UnitsInStock}", state:"Error"}),
                             contentRigth: new sap.m.Label({text:"{parts: [ {path:'odataNorthwind>UnitPrice'},{path:'odataNorthwind>Currency'} ], type: 'sap.ui.model.type.Currency'}"})
                         })
                     ]
@@ -60,6 +87,35 @@ sap.ui.define([
                 return customListItem;
             }
 
+        },
+
+        onSaveSignature: function(oEvent){
+            const signature = this.byId("signature");
+            const oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+            let signaturePng;
+
+            if(!signature.isFill()){
+                MessageBox.error(oResourceBundle.getText("fillSignature"));
+            } else {
+                signaturePng = signature.getSignature().replace("data:image/png;base64,","");
+                let objectOrder = oEvent.getSource().getBindingContext("odataNorthwind").getObject();
+                let body = {
+                    OrderId: objectOrder.OrderID.toString(),
+                    SapId: this.getOwnerComponent().SapId,
+                    EmployeeId: objectOrder.EmployeeID.toString(),
+                    MimeType: "image/png",
+                    MediaContent: signaturePng
+                };
+
+                this.getView().getModel("incidenceModel").create("/SignatureSet", body, {
+                    success: function() {
+                        MessageBox.information(oResourceBundle.getText("signatureSaved"));
+                    },
+                    error: function() {
+                        MessageBox.error(oResourceBundle.getText("signatureNotSaved"));
+                    }
+                })
+            };
         }
     });
 });
