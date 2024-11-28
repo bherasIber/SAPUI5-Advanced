a8b209e64dca49356d3f04da58d3510efe0a878d
bherasIber <bheras@ibercacao.es>
2024-11-28 15:27:42 +0000

    Ultima

--- Employees/webapp/controller/OrderDetails.controller.js
+++ Employees/webapp/controller/OrderDetails.controller.js
@@ -167,5 +167,23 @@ function (Controller, History, MessageBox, Filter, FilterOperator) {
         onFileUploadComplete: function(oEvent){
             oEvent.getSource().getBinding("items").refresh();
         },
+
+        onFileDeleted: function(oEvent){
+            var oUploadCollection = oEvent.getSource();
+            var sPath = oEvent.getParameter("item").getBindingContext("incidenceModel").getPath();
+            this.getView().getModel("incidenceModel").remove(sPath, {
+                success: function() {
+                    oUploadCollection.getBinding("items").refresh();
+                },
+                error: function () {
+
+                }
+            });
+        },
+
+        downloadFile: function(oEvent) {
+            const sPath = oEvent.getSource().getBindingContext("incidenceModel").getPath();
+            window.open("/sap/opu/odata/sap/YSAPUI5_SRV_01" + sPath + "/$value");
+        },
     });
 });
